17c686bb9e483cc83ae66e374a8db226b80db11d
Gary950312 <gary20060312@gmail.com>
2024-09-27 08:55:29 +0000

    123

--- 學校上課/HW10.java
+++ 學校上課/HW10.java
@@ -4,31 +4,10 @@ public class HW10{
         java.util.Scanner input=new java.util.Scanner(System.in);
         int num;
 
-        System.out.print("絕對值計算/n===========================/n請輸入一個整數:");
+        System.out.print("絕對值計算\n===========================\n請輸入一個整數:");
         num=input.nextInt();
 
-
-
-
-
-
-
-
-
+        System.out.println("|"+num+"|="+Math.abs(num));
 
    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
