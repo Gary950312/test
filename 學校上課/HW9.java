ef87dc611cf9f4b16931c2b015c50a25751099ee
Gary950312 <gary20060312@gmail.com>
2024-09-27 08:05:35 +0000

    HW9

---
+++ 學校上課/HW9.java
@@ -0,0 +1,23 @@
+public class HW9{
+
+    public static void main(String[] args)
+      {
+
+        java.util.Scanner input=new java.util.Scanner(System.in);
+          int num;
+
+        System.out.print("請輸入一個整數:");
+        num=input.nextInt();
+        
+        if(num%2==0)
+         {
+            System.out.print(num+"是偶數");
+         }
+        if(num%2!=0)
+         {
+            System.out.print(num+"是奇數");
+         }
+
+      }
+
+}
